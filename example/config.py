2dce042f8a5b3c15764d21476a9684d48ca0efca
Aaron Arredondo Sanchez <binauralvoice@gmail.com>
2017-06-21 13:06:43 -0500

    el / de mas SERVER_URL

--- example/config.py
+++ example/config.py
@@ -1,5 +1,5 @@
 CONFIG = {
     'FACEBOOK_TOKEN': 'EAAE9pkz7ZBQQBAFnDxMvQsS0KAnASAZBlzQvartTSGgmrixJiWZBOJlfYuCa6NwQUD3hcJmqi25ZCWwcjPeZAZBMrY3vLqLO9K4wRp7lOSoizD44dxKRWDLZCjhOJgAvNKixMlbh1ZAkVyTjD7Oy8ZCYvP7P6S2mMp7FZBrceUyfprBwZDZD',
     'VERIFY_TOKEN': 'verify_gbmbot',
-    'SERVER_URL': 'https://gbm-but.herokuapp.com/'
+    'SERVER_URL': 'https://gbm-but.herokuapp.com'
 }
