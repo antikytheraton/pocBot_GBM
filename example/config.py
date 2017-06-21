00e1d336c0e024416593bc86e40b650f080bfac7
Aaron Arredondo Sanchez <binauralvoice@gmail.com>
2017-06-20 23:24:08 -0500

    heroku configuration

--- example/config.py
+++ example/config.py
@@ -1,5 +1,5 @@
 CONFIG = {
-    'FACEBOOK_TOKEN': 'EAANgzn2wUtoBADwVamrsDaGZBRrFiEKz7UXtpALkmiH1WNzBPZCtbvbSsDGGZAbmBbdJZChg7ivMMdURQAwiRCsoiB41LDC8haMUzSutGti88ebZBx5smzLpR0xMzjlsfetrrCdWdcjTq8Onhr0bbZB11l67YKuV0OpQpalmFgLQZDZD',
-    'VERIFY_TOKEN': 'verify_simsimbot',
-    'SERVER_URL': 'https://karl.ohrocks.com'
+    'FACEBOOK_TOKEN': 'EAAEDZBS8YBFMBANnOtzxB3DOFZC92UjhMw2pCBxa4jy2jFjwbo2v3rYcZAZAgzxZCGBARdU6H3MMkVh7hTCeyqqPZBvcWw0KmZC3T7ikjZA1eh4HS329h9xAFnoGIuvi0diL8Bz5i7WUWb5gAk1fJHuXQEUPmAvqBmmHwKAdTxnJ2AZDZD',
+    'VERIFY_TOKEN': 'verify_gbmbot',
+    'SERVER_URL': 'https://pocbot-gbm.herokuapp.com/'
 }
