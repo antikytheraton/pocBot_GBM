43009451084d8984d53c68a05c5a0822bc60f347
Aaron Arredondo Sanchez <binauralvoice@gmail.com>
2017-06-21 11:34:15 -0500

    heroku fly again test 1

--- example/config.py
+++ example/config.py
@@ -1,5 +1,5 @@
 CONFIG = {
-    'FACEBOOK_TOKEN': 'xxx',
-    'VERIFY_TOKEN': 'verifygbmbot',
-    'SERVER_URL': 'https://fdb04ecb.ngrok.io'
+    'FACEBOOK_TOKEN': 'EAAbHmTNfZCUwBAJMFDdAztNwZAb0ZCoLNV1gtDpcqkhZCZB2nETqZBp4ZCyhAG8TkVpkbfyLeiKmZARoZCB0VH9RsIl6IC45bOY55g5u0lx1oXEthf6CXiZBZCcQ3Kn0Oyd1lDfpAxZBAqF1nHrYWwD9f5lKrKmcElfeS1NwtawZAVVXgZAAZDZD',
+    'VERIFY_TOKEN': 'verify_gbmbot',
+    'SERVER_URL': 'https://pocbot-gbm.herokuapp.com/'
 }
